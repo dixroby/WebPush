70d8f5a7ed9a10c2758371ad7fabb7fe9fd84813
dixroby <dixroby.arone@touchconsulting.pe>
2023-08-24 22:51:36 -0500

    Refactor: Se configura para que el proyecto web sea PWA.

--- WebPush.Client/wwwroot/service-worker.js
+++ WebPush.Client/wwwroot/service-worker.js
@@ -2,3 +2,8 @@
     console.log('Intalando el service worker...');
     self.skipWaiting();
 });
+
+//una forma de saltarse para que la app sea offline, uno de los requisitos que pide PWA
+self.addEventListener('fetch', event => {
+    return null;
+})
